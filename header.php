2920eb97d3080a590da0904d8581f96fbc226fa9
Mijanur Rahman <mahmudmizan717@gmail.com>
2023-11-09 20:59:25 +0600

    Complete

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            mizan42047
+ * Author:            Hemanth Sharma
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       header
@@ -35,6 +35,7 @@ function create_block_toehold_block_init()
     foreach ($blocks as $block) {
         register_block_type(plugin_dir_path(__FILE__) . 'build/blocks/' . $block);
     }
+    wp_register_style("google-fonts", "https://fonts.googleapis.com/css2?family=Figtree:wght@400;500;600&family=Plus+Jakarta+Sans:wght@400;700&display=swap", array(), "1.0.0", "all");
 }
 
 add_action('init', 'create_block_toehold_block_init');
